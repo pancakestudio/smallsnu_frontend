92c06a4b372c0d4d85f984a9bc34413489840aec
박종석 <jsp1256@gmail.com>
2019-04-24 03:13:21 +0900

    render header and map in main page

---
+++ src/components/pages/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Map from '../organisms/Map'
+import Header from '../organisms/Header'
+import './Main.css'
+
+const Main = () => (
+  <div className = "MainPage">
+    <Header className = "Header"/>
+    <Map className = "Map"/>
+  </div>
+)
+
+export default Main
