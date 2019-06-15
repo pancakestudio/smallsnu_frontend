dfeccdb9a824569382f8f332a8404e2629511103
sckimynwa <tsb03672@naver.com>
2019-06-15 11:39:37 +0900

    bug fix & remove reservation from sidebar

--- src/components/organisms/Sidebar.js
+++ src/components/organisms/Sidebar.js
@@ -51,7 +51,6 @@ export const Sidebar = ({show, onResClick, onSemiClick, onCafeClick, onConvClick
       <ListGroup variant="flush">
         <ListGroup.Item className="bg-light" action active={false}>길찾기</ListGroup.Item>
         <ListGroup.Item className="bg-light" action active={false}>셔틀 버스</ListGroup.Item>
-        <ListGroup.Item className="bg-light" action active={false}>강의실 예약</ListGroup.Item>
         <ListGroup.Item className="bg-light" action active={false} onClick={handleSemiClick}>세미나</ListGroup.Item>
         <ListGroup.Item className="bg-light" action active={false} onClick={handleResClick}>식당</ListGroup.Item>
         <ListGroup.Item className="bg-light" action active={false} onClick={handleCafeClick}>카페</ListGroup.Item>
